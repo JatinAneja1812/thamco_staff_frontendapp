0b8f2f0950265820dc3a9357f9c5682bece3274e
Jatin Aneja <jatinaneja20000@gmail.com>
2023-12-16 09:02:22 +0000

    Added products mock in presentation layer

--- src/Components/Pages/AllProductsCategories/AllProductsCategories.js
+++ src/Components/Pages/AllProductsCategories/AllProductsCategories.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Container, Fade } from "@mui/material";
+import { Button, Container, Fade } from "@mui/material";
 import CategoryCard from "../../Cards/ProductsCategoryCards/CategoriesCards";
 import allCategories from "../../../Utility/Categores/CategoriesList";
 import {
@@ -7,17 +7,34 @@ import {
   PageTitle,
   CategoriesContainer,
 } from "./AllProductsCategories.styles";
+import { useNavigate } from "react-router";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useAuth } from "../../../Hooks/Authentication/AuthProvider";
+import HomeIcon from '@mui/icons-material/Home';
 
 const AllProductsCategories = () => {
-  
+
+  const navigate = useNavigate();
+  const {isAuthenticated} = useAuth();
+
   useEffect(() => {
     window.scroll({ top: 0 });
   }, []);
+
+  const handleReturn = () => {
+    if(!isAuthenticated){
+      navigate("/");
+    }
+    else{
+      navigate("/home");
+    }
+  }
 
   return (
     <ProductsCategoriesContainerWrapper>
       <Fade in={true}>
         <Container>
+          <Button startIcon={<ArrowBackIcon />} onClick={() => handleReturn()} style={{backgroundColor: "#437046", color: "#fff"}} endIcon={<HomeIcon/>} ></Button>
           {/* Title */}
           <PageTitle>All Categories</PageTitle>
           {/* All Category Cards */}
